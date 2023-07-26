7968877beb1f6dd9aa1600480dbf56302baa45ee
Pirito10 <aaronriveiro@gmail.com>
2023-07-26 02:31:11 +0200

    create 10-rock-paper-scissors.js (chapter 10)

---
+++ scripts/10-rock-paper-scissors.js
@@ -0,0 +1,70 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+updateScoreElement()
+
+function playGame(playerMove) {
+    const computerMove = pickComputerMove()
+    let result = ''
+
+    if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+            result = 'You lose.'
+        } else if (computerMove === 'paper') {
+            result = 'You win.'
+        } else if (computerMove === 'scissors') {
+            result = 'Tie.'
+        }
+    } else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            result = 'You win.'
+        } else if (computerMove === 'paper') {
+            result = 'Tie.'
+        } else if (computerMove === 'scissors') {
+            result = 'You lose.'
+        }
+    } else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie.'
+        } else if (computerMove === 'paper') {
+            result = 'You lose.'
+        } else if (computerMove === 'scissors') {
+            result = 'You win.'
+        }
+    }
+
+    if (result === 'You win.') {
+        score.wins += 1
+    } else if (result === 'You lose.') {
+        score.losses += 1
+    } else if (result === 'Tie.') {
+        score.ties += 1
+    }
+
+    localStorage.setItem('score', JSON.stringify(score))
+
+    updateScoreElement()
+
+    document.querySelector('.js-result').innerHTML = result
+
+    document.querySelector('.js-moves').innerHTML = `You <img class="move-icon" src="images/${playerMove}-emoji.png"><img class="move-icon" src="images/${computerMove}-emoji.png"> Computer`
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+}
+
+function pickComputerMove() {
+    const randomNumber = Math.random()
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        return 'rock'
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        return 'paper'
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        return 'scissors'
+    }
+}
