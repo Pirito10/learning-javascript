a8b0bd51bed109ea1d1475647266540eb092ad95
Pirito10 <aaronriveiro@gmail.com>
2023-07-28 02:05:38 +0200

    update 11-todo-list.js (chapter 11)

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,4 @@
-const todoList = []
+const todoList = [{ name: 'make dinner', dueDate: '2022-12-22' }, { name: 'wash dishes', dueDate: '2022-12-22' }]
 
 renderTodoList()
 
@@ -6,11 +6,11 @@ function renderTodoList() {
     let todoListHTML = ''
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i]
-        const html = `<p>${todo}</p>`
+        const todoObject = todoList[i]
+        const { name, dueDate } = todoObject
+        const html = `<div>${name}</div> <div>${dueDate}</div> <button class="delete-todo-button" onclick="todoList.splice(${i}, 1); renderTodoList()">Delete</button>`
         todoListHTML += html
     }
-    console.log(todoListHTML)
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML
 }
@@ -19,8 +19,10 @@ function addTodo() {
     const inputElement = document.querySelector('.js-name-input')
     const name = inputElement.value
 
-    todoList.push(name)
-    console.log(todoList)
+    const dateInputElement = document.querySelector('.js-due-date-input')
+    const dueDate = dateInputElement.value
+
+    todoList.push({ name, dueDate })
 
     inputElement.value = ''
 
